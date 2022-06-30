da9641da3933486b04c1101c59be9f6b43edf491
Seyitanx <seyitanadepegba@gmail.com>
2022-06-30 20:40:11 +0100

    created a new component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 
-import './App.css';
+import "./styles.css";
 import Weather from './weather';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        
-        <h1>
-          hello world!!
-        </h1>
+    <div >
+     
+      
         <Weather />
-      </header>
+     
     </div>
   );
 }
--- src/weather.js
+++ src/weather.js
@@ -1,7 +1,68 @@
 import React from "react";
 
+import "./styles.css";
+
 export default function Weather() {
-    return(
-        <h1>hello from weather</h1>
-    )
+  return (
+    <div className="container">
+      <div className="card-cover">
+        <div className="back-card">
+          <form id="search-form">
+            <input
+              type="text"
+              placeholder="Enter City"
+              autofocus="on"
+              id="search-input"
+            />
+            <input
+              type="submit"
+              value="Search"
+              className="button"
+              id="new-button"
+            />
+            <button type="button" className="second" id="current-button">
+              Show current location
+            </button>
+          </form>
+          <div className="weather-overview">
+     < div id="current-city"></div>
+            <ul>
+              <li id="the-date"></li>
+              <li id="cloud-description"></li>
+            </ul>
+          </div>
+
+          <div className="row">
+            <div className="col-sm-6">
+              <div className="weather-temperature">
+                <ul>
+                  <li>
+                    <strong id="current-degree"></strong>
+                    <span className="units">℃</span>
+                  </li>
+                </ul>*
+                <img
+                  src=""
+                  id="icon"
+                  className="cloud-icon"
+                  alt="weather-icon"
+                />
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <ul>
+                <li>
+                  Humidity: <span id="humidity"></span>%
+                </li>
+                <li>
+                  Wind: <span id="wind"></span>m/h
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
