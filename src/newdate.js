0c986086f2c40e3945a36553330d46d448944b22
Seyitanx <seyitanadepegba@gmail.com>
2022-07-07 23:15:10 +0100

    .

--- src/newdate.js
+++ src/newdate.js
@@ -1,7 +1,29 @@
 import React from "react";
 import Weather from "./weather";
 export default function NewDate(props) {
+  
+  
+ 
+   
+  let day = days[props.getDay()];
+  let hour = props.getHour();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+let days =[
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+   ]
     return (
-      <div></div>  
+      <div> {day}, {hour}:{minutes}</div>  
     )
 }
