7aab08daccdb95a265f806c341e8ae00b85eb275
Seyitanx <seyitanadepegba@gmail.com>
2022-07-14 19:54:59 +0100

    .

--- src/weatherforecast.js
+++ src/weatherforecast.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import WeatherIcon from "./weathericon";
+
 import DailyForecast from "./dailyforecast";
 import axios from "axios";
 import "./WeatherForecast.css";
